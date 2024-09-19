da3f5fb5fb687d86512f72883ff37492e6ea24cb
Luis Infante <113048160+LuisMInfante@users.noreply.github.com>
2024-09-19 19:07:59 -0400

    testing producer with ticker

--- Tech-Lab-On-Campus/Topic-Exchange/publish.py
+++ Tech-Lab-On-Campus/Topic-Exchange/publish.py
@@ -25,7 +25,7 @@ def main(ticker: str, price: float, sector: str) -> None:
     #                       WRITE CODE HERE!!!
     #
 
-
+    routingKey = f"#.{sector}.#"
     producer = mqProducer(routing_key=routingKey,exchange_name="Tech Lab Topic Exchange")
 
 
@@ -33,7 +33,7 @@ def main(ticker: str, price: float, sector: str) -> None:
     #
     #                       WRITE CODE HERE!!!
     #
-    
+    message = f"{ticker} price is now ${price}"
     
     producer.publishOrder(message)
 
@@ -43,5 +43,9 @@ if __name__ == "__main__":
     #
     #                       WRITE CODE HERE!!!
     #
+    ticker = sys.argv[1]
+    price = sys.argv[2]
+    sector = sys.argv[3]
+
 
     sys.exit(main(ticker,price,sector))
